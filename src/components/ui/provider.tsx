2c0ab89a53430e22c914e93b93b80876467df39f
Emanuel Kagombora <emanuelkagombora28@gmail.com>
2025-06-10 14:53:30 +0300

    feat: implement theme feature in the app

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -2,12 +2,16 @@
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { ColorModeProvider } from "./color-mode";
+import { Toaster } from "./toaster";
 import { system } from "../../theme";
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <ChakraProvider value={system}>
-      <ColorModeProvider defaultTheme="dark">{children}</ColorModeProvider>
+      <ColorModeProvider defaultTheme="dark">
+        {children}
+        <Toaster />
+      </ColorModeProvider>
     </ChakraProvider>
   );
 }
